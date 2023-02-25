ebcebd95e03867a10932a87e7c4651244b4bd07a
Raed <78278357+ElMajdoubraed@users.noreply.github.com>
2023-02-25 22:38:14 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,11 @@ const express = require('express')
 const server = express()
 
 server.get('/api', (req, res) => {
-    res.send('Hello from the backend!')
+    res.send('Hello from the backend test1!')
 })
 
-app.listen(5000)
+server.get('/', (req, res) => {
+    res.send('Hello from the backend test1 ...!')
+})
+
+server.listen(5000)
